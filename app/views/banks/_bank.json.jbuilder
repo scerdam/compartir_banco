4ec4622c9606d039ad62357b9fef14ab2e600108
Sebastian Cerda <sebastian.cerda@ssindex.org>
2023-10-29 22:15:07 -0300

    feat(models>bank): ADD Bank scaffold

---
+++ app/views/banks/_bank.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! bank, :id, :name, :code, :created_at, :updated_at
+json.url bank_url(bank, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :banks
   resources :accounts
   resources :users
   #root to: ''
