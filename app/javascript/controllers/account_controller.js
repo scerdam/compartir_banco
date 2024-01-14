d306ff34465610eea561f4c4801617110e464a21
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-01-13 22:12:54 -0300

    feat(javascript>account): ADD function for copy account details to user clipboard

--- app/javascript/controllers/account_controller.js
+++ app/javascript/controllers/account_controller.js
@@ -2,8 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="account"
 export default class extends Controller {
-  //static targets = ["btn"]
-  copy() {
-    console.log("Hello, Stimulus!")
+  static targets = ["account"]
+  
+  copy = async () => {
+    try {
+      await navigator.clipboard.writeText(
+        `Sebastian Cerda
+        98.347.633-5
+        Banco de Chile
+        Cuenta Corriente
+        00-346-21234-09
+        sebastian.cerda@compartebanco.cl`);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
   }
 }
